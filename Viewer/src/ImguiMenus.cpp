c416af88978fb9e656780d5be3224fc57b3ad33b
Liraneve91 <liraneve@gmail.com>
2018-11-13 16:29:17 +0200

    GUI changed

--- Viewer/src/ImguiMenus.cpp
+++ Viewer/src/ImguiMenus.cpp
@@ -17,6 +17,10 @@
 bool showDemoWindow = false;
 bool showAnotherWindow = false;
 
+bool isTranslation = true;
+bool isRotation = false;
+bool isScaling = false;
+
 glm::vec4 clearColor = glm::vec4(0.8f, 0.8f, 0.8f, 1.00f);
 
 const glm::vec4& GetClearColor()
@@ -58,31 +62,55 @@ void DrawImguiMenus(ImGuiIO& io, Scene& scene)
 			ImGui::Combo("Select Model", &scene.activeModelIndex, items, modelsNumber); 
 			ImGui::Combo("Select Camera", &scene.activeCameraIndex, cameras, cameraCount);
 
+
+			//ImGui::RadioButton("ModelRotation", &isTranslation);
+			//ImGui::RadioButton("ModelTranslation", &isRotation);
+			//ImGui::RadioButton("ModelScaling", &isScaling);
+
+			static int e = 0;
+			ImGui::RadioButton("ModelRotation", &e, 0); ImGui::SameLine();
+			ImGui::RadioButton("ModelTranslation", &e, 1); ImGui::SameLine();
+			ImGui::RadioButton("ModelScaling", &e, 2);
+
+
 			/** Active model parameters ***/
 			MeshModel* activeModel = scene.GetActiveModel();
 			ImGui::Text("Model parameters");
-			ImGui::SliderFloat("Model X rotation", &(activeModel->param->rot_x), -360.0f, 360.0f);
-			ImGui::SliderFloat("Model Y rotation", &(activeModel->param->rot_y), -360.0f, 360.0f);
-			ImGui::SliderFloat("Model Z rotation", &(activeModel->param->rot_z), -360.0f, 360.0f);
-
-			ImGui::SliderFloat("Model X translation", &(activeModel->param->trans_x), -100.0f, 100.0f);
-			ImGui::SliderFloat("Model Y translation", &(activeModel->param->trans_y), -100.0f, 100.0f);
-			ImGui::SliderFloat("Model Z translation", &(activeModel->param->trans_z), -100.0f, 100.0f);
-
-			ImGui::SliderFloat("Model X scaling", &(activeModel->param->scale_x), 0.1f, 100.0f);
-			ImGui::SliderFloat("Model Y scaling", &(activeModel->param->scale_y), 0.1f, 100.0f);
-			ImGui::SliderFloat("Model Z scaling", &(activeModel->param->scale_z), 0.1f, 100.0f);
-			
+			if (e==0)
+			{
+				ImGui::SliderFloat("Model X rotation", &(activeModel->param->rot_x), -360.0f, 360.0f);
+				ImGui::SliderFloat("Model Y rotation", &(activeModel->param->rot_y), -360.0f, 360.0f);
+				ImGui::SliderFloat("Model Z rotation", &(activeModel->param->rot_z), -360.0f, 360.0f);
+			}
+			else if (e==1) {
+				ImGui::SliderFloat("Model X translation", &(activeModel->param->trans_x), -100.0f, 100.0f);
+				ImGui::SliderFloat("Model Y translation", &(activeModel->param->trans_y), -100.0f, 100.0f);
+				ImGui::SliderFloat("Model Z translation", &(activeModel->param->trans_z), -100.0f, 100.0f);
+			}
+			else if (e==2)
+			{
+				ImGui::SliderFloat("Model X scaling", &(activeModel->param->scale_x), 0.1f, 100.0f);
+				ImGui::SliderFloat("Model Y scaling", &(activeModel->param->scale_y), 0.1f, 100.0f);
+				ImGui::SliderFloat("Model Z scaling", &(activeModel->param->scale_z), 0.1f, 100.0f);
+			}
 			/** Active camera parameters ***/
 			Camera* activeCamera = scene.GetActiveCamera();
 			ImGui::Text("Camera parameters");
-			ImGui::SliderFloat("Camera X rotation", &(activeCamera->param->rot_x), -360.0f, 360.0f);
-			ImGui::SliderFloat("Camera Y rotation", &(activeCamera->param->rot_y), -360.0f, 360.0f);
-			ImGui::SliderFloat("Camera Z rotation", &(activeCamera->param->rot_z), -360.0f, 360.0f);
-
-			ImGui::SliderFloat("Camera X translation", &(activeCamera->param->trans_x), -100.0f, 100.0f);
-			ImGui::SliderFloat("Camera Y translation", &(activeCamera->param->trans_y), -100.0f, 100.0f);
-			ImGui::SliderFloat("Camera Z translation", &(activeCamera->param->trans_z), -100.0f, 100.0f);
+			static int c = 0;
+			ImGui::RadioButton("ModelRotation", &c, 0); ImGui::SameLine();
+			ImGui::RadioButton("ModelTranslation", &c, 1); 
+			if (c == 0)
+			{
+				ImGui::SliderFloat("Camera X rotation", &(activeCamera->param->rot_x), -360.0f, 360.0f);
+				ImGui::SliderFloat("Camera Y rotation", &(activeCamera->param->rot_y), -360.0f, 360.0f);
+				ImGui::SliderFloat("Camera Z rotation", &(activeCamera->param->rot_z), -360.0f, 360.0f);
+			}
+			else if (c == 1)
+			{
+				ImGui::SliderFloat("Camera X translation", &(activeCamera->param->trans_x), -100.0f, 100.0f);
+				ImGui::SliderFloat("Camera Y translation", &(activeCamera->param->trans_y), -100.0f, 100.0f);
+				ImGui::SliderFloat("Camera Z translation", &(activeCamera->param->trans_z), -100.0f, 100.0f);
+			}
 		}
 		ImGui::End();
 	}
