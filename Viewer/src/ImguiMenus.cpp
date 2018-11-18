fc21ec5a19399b8411a52e54af96b8a80adb8c99
YairMarom <yairmrm@gmail.com>
2018-11-18 18:43:24 +0200

    Finished menus. Ready for submission!!

--- Viewer/src/ImguiMenus.cpp
+++ Viewer/src/ImguiMenus.cpp
@@ -35,7 +35,7 @@ void DrawImguiMenus(ImGuiIO& io, Scene& scene)
 	// 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
 	{
 		bool isAddCmaera = false;
-		ImGui::Begin("Computer Graphic ass. 1");                         
+		ImGui::Begin("Computer Graphic ass. 1");
 		std::vector<std::string> modelNames = scene.getModelNames();
 		std::vector<std::string> cameraNames = scene.GetCameraNames();
 		int modelsNumber = modelNames.size();
@@ -48,74 +48,81 @@ void DrawImguiMenus(ImGuiIO& io, Scene& scene)
 				items[i] = const_cast<char*>(modelNames[i].c_str());
 			}
 			char** cameras = new char*[cameraCount];
-			for (int i=0 ; i< cameraCount ;++i)
+			for (int i = 0; i < cameraCount; ++i)
 			{
 				cameras[i] = const_cast<char*>(cameraNames[i].c_str());
 			}
 
-			scene.isAddCamera = ImGui::Button("AddCamera", ImVec2(70, 30));
-
-			ImGui::Checkbox("Prespective Projection", &scene.isPrespective);
-
-			ImGui::Combo("Select Model", &scene.activeModelIndex, items, modelsNumber); 
-			ImGui::Combo("Select Camera", &scene.activeCameraIndex, cameras, cameraCount);
-
-			static int e = 2;
-			ImGui::RadioButton("ModelRotation", &e, 0); ImGui::SameLine();
-			ImGui::RadioButton("ModelTranslation", &e, 1); ImGui::SameLine();
-			ImGui::RadioButton("ModelScaling", &e, 2);
 
 
-			/** Active model parameters ***/
-			MeshModel* activeModel = scene.GetActiveModel();
-			ImGui::Text("Model parameters");
-			if (e==0)
-			{
-				ImGui::SliderFloat("Model X rotation", &(activeModel->param->rot_x), -360.0f, 360.0f);
-				ImGui::SliderFloat("Model Y rotation", &(activeModel->param->rot_y), -360.0f, 360.0f);
-				ImGui::SliderFloat("Model Z rotation", &(activeModel->param->rot_z), -360.0f, 360.0f);
-			}
-			else if (e==1) {
-				ImGui::SliderFloat("Model X translation", &(activeModel->param->trans_x), -1000.0f, 1000.0f);
-				ImGui::SliderFloat("Model Y translation", &(activeModel->param->trans_y), -1000.0f, 1000.0f);
-				ImGui::SliderFloat("Model Z translation", &(activeModel->param->trans_z), -1000.0f, 1000.0f);
-			}
-			else if (e==2)
-			{
-				ImGui::SliderFloat("Model X scaling", &(activeModel->param->scale_x), 0.1f, 100.0f);
-				ImGui::SliderFloat("Model Y scaling", &(activeModel->param->scale_y), 0.1f, 100.0f);
-				ImGui::SliderFloat("Model Z scaling", &(activeModel->param->scale_z), 0.1f, 100.0f);
-			}
-			/** Active camera parameters ***/
-			Camera* activeCamera = scene.GetActiveCamera();
-			ImGui::Text("Camera parameters");
-			static int c = 0;
-			ImGui::RadioButton("CmaeraRotation", &c, 0); ImGui::SameLine();
-			ImGui::RadioButton("CmaeraTranslation", &c, 1); 
-			if (c == 0)
-			{
-				ImGui::SliderFloat("Camera X rotation", &(activeCamera->param->rot_x), -360.0f, 360.0f);
-				ImGui::SliderFloat("Camera Y rotation", &(activeCamera->param->rot_y), -360.0f, 360.0f);
-				ImGui::SliderFloat("Camera Z rotation", &(activeCamera->param->rot_z), -360.0f, 360.0f);
-			}
-			else if (c == 1)
-			{
-				ImGui::SliderFloat("Camera X translation", &(activeCamera->param->trans_x), -1000.0f, 1000.0f);
-				ImGui::SliderFloat("Camera Y translation", &(activeCamera->param->trans_y), -1000.0f, 1000.0f);
-				ImGui::SliderFloat("Camera Z translation", &(activeCamera->param->trans_z), -1000.0f, 1000.0f);
-			}
-
-			ImGui::Checkbox("Show Face normals", &scene.isShowFaceNormals); ImGui::SameLine();
-			ImGui::Checkbox("Show Vertex normals", &scene.isShowVertexNormals);
-			ImGui::SliderFloat("Normal scaling", &(scene.scaleNormalLength),0.001,2);
-			ImGui::SliderFloat("Zoom", &(scene.zoom),1,10);
-			ImGui::SliderFloat("Distance", &(activeCamera->distance),0,10);
-			ImGui::ColorEdit3("Model color", (float*)&(scene.modelColor)); 
-			ImGui::ColorEdit3("Grid color", (float*)&(scene.gridColor)); 
-			ImGui::ColorEdit3("Normals color", (float*)&(scene.normalsColor)); 
-			ImGui::ColorEdit3("Bounding Box color", (float*)&(scene.boundingBoxColor)); 
+			ImGui::ColorEdit3("Model color", (float*)&(scene.modelColor));
+			ImGui::ColorEdit3("Grid color", (float*)&(scene.gridColor));
+			ImGui::ColorEdit3("Normals color", (float*)&(scene.normalsColor));
+			ImGui::ColorEdit3("Bounding Box color", (float*)&(scene.boundingBoxColor));
 			ImGui::ColorEdit3("Background color color", (float*)&(scene.beackgroundColor));
-			
+
+			ImGui::Combo("Select Model", &scene.activeModelIndex, items, modelsNumber);
+			ImGui::Combo("Select Camera", &scene.activeCameraIndex, cameras, cameraCount);
+			if (ImGui::CollapsingHeader("Model"))
+			{
+				static int e = 2;
+				ImGui::RadioButton("ModelRotation", &e, 0); ImGui::SameLine();
+				ImGui::RadioButton("ModelTranslation", &e, 1); ImGui::SameLine();
+				ImGui::RadioButton("ModelScaling", &e, 2);
+
+				ImGui::Checkbox("Show Face normals", &scene.isShowFaceNormals); ImGui::SameLine();
+				ImGui::Checkbox("Show Vertex normals", &scene.isShowVertexNormals);
+				ImGui::SliderFloat("Normal scaling", &(scene.scaleNormalLength), 0.001, 2);
+
+
+				/** Active model parameters ***/
+				MeshModel* activeModel = scene.GetActiveModel();
+				ImGui::Text("Model parameters");
+				if (e == 0)
+				{
+					ImGui::SliderFloat("Model X rotation", &(activeModel->param->rot_x), -360.0f, 360.0f);
+					ImGui::SliderFloat("Model Y rotation", &(activeModel->param->rot_y), -360.0f, 360.0f);
+					ImGui::SliderFloat("Model Z rotation", &(activeModel->param->rot_z), -360.0f, 360.0f);
+				}
+				else if (e == 1) {
+					ImGui::SliderFloat("Model X translation", &(activeModel->param->trans_x), -1000.0f, 1000.0f);
+					ImGui::SliderFloat("Model Y translation", &(activeModel->param->trans_y), -1000.0f, 1000.0f);
+					ImGui::SliderFloat("Model Z translation", &(activeModel->param->trans_z), -1000.0f, 1000.0f);
+				}
+				else if (e == 2)
+				{
+					ImGui::SliderFloat("Model X scaling", &(activeModel->param->scale_x), 0.1f, 100.0f);
+					ImGui::SliderFloat("Model Y scaling", &(activeModel->param->scale_y), 0.1f, 100.0f);
+					ImGui::SliderFloat("Model Z scaling", &(activeModel->param->scale_z), 0.1f, 100.0f);
+				}
+			}
+
+			if (ImGui::CollapsingHeader("Camera"))
+			{
+				scene.isAddCamera = ImGui::Button("AddCamera", ImVec2(70, 30));
+				/** Active camera parameters ***/
+				Camera* activeCamera = scene.GetActiveCamera();
+				ImGui::Text("Camera parameters");
+				ImGui::Checkbox("Prespective Projection", &scene.isPrespective);
+				static int c = 0;
+				ImGui::RadioButton("CmaeraRotation", &c, 0); ImGui::SameLine();
+				ImGui::RadioButton("CmaeraTranslation", &c, 1);
+				if (c == 0)
+				{
+					ImGui::SliderFloat("Camera X rotation", &(activeCamera->param->rot_x), -360.0f, 360.0f);
+					ImGui::SliderFloat("Camera Y rotation", &(activeCamera->param->rot_y), -360.0f, 360.0f);
+					ImGui::SliderFloat("Camera Z rotation", &(activeCamera->param->rot_z), -360.0f, 360.0f);
+				}
+				else if (c == 1)
+				{
+					ImGui::SliderFloat("Camera X translation", &(activeCamera->param->trans_x), -1000.0f, 1000.0f);
+					ImGui::SliderFloat("Camera Y translation", &(activeCamera->param->trans_y), -1000.0f, 1000.0f);
+					ImGui::SliderFloat("Camera Z translation", &(activeCamera->param->trans_z), -1000.0f, 1000.0f);
+				}
+				ImGui::SliderFloat("Zoom", &(scene.zoom), 1, 10);
+				ImGui::SliderFloat("Distance", &(activeCamera->distance), 0, 10);
+			}
+
 
 		}
 		ImGui::End();
